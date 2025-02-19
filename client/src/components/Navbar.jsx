6f7165bf786ef058981cf6be34309d2630bb8212
JayaNirenjan45 <jayanirenjan@gmail.com>
2025-02-19 22:49:24 +0530

    Updated navbar and added userprofile

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import iconlogo from "../assets/download.jpg";
 import "./Navbar.css";
 
 function Navbar() {
+  const location = useLocation(); // Get current path
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -14,16 +16,16 @@ function Navbar() {
       <div className="navbar-right">
         <ul>
           <li>
-            <Link to="/suggestions">Suggestions</Link>
+            <Link to="/suggestions" className={location.pathname === "/suggestions" ? "active" : ""}>Suggestions</Link>
           </li>
           <li>
-            <Link to="/attendance">Attendance</Link>
+            <Link to="/attendance" className={location.pathname === "/attendance" ? "active" : ""}>Attendance</Link>
           </li>
           <li>
-            <Link to="/userprofile">User Profile</Link>
+            <Link to="/userprofile" className={location.pathname === "/userprofile" ? "active" : ""}>User Profile</Link>
           </li>
           <li>
-            <Link to="/diet">Diet</Link>
+            <Link to="/diet" className={location.pathname === "/diet" ? "active" : ""}>Diet</Link>
           </li>
         </ul>
       </div>
